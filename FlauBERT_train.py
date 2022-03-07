4f24fa97ab949554d8695b2e3898920cf0b19bac
ArthurVanSchendel <arthur.vanschendel15@gmail.com>
2022-03-07 14:45:45 +0100

    fix: data collector module

--- FlauBERT_train.py
+++ FlauBERT_train.py
@@ -1,3 +1,4 @@
+from posixpath import join
 import torch
 import transformers
 import numpy as np
@@ -42,87 +43,94 @@ def group_texts(examples):
     result["labels"] = result["input_ids"].copy()
     return result
 
-def raw_txt_to_txt(filename, data_type, output_name):
-  file = open(filename, 'rt')
-  text = file.readlines()
-  file.close()
-  text = np.array(text)
-  print("TEXT SHAPE = ", text.shape)
-
-  txt_data = pd.DataFrame(columns=['aggregate'])
-  aggregates = []
-  for i in range(text.shape[0]):
-    if text[i] == "\n":
-      continue
-    splitted_text = text[i].split(";")
-    splitted_text = np.array(splitted_text)
-    splitted_text[0] = splitted_text[0].strip()
-    splitted_text[0] = splitted_text[0] + '.'
-    aggregates.append(splitted_text[0])
-  txt_data['aggregates'] = aggregates
-  txt = txt_data.to_csv(f"{output_name}.txt", encoding="utf-8-sig")
-  return txt
-
 
 def extract_txt_from_html(url):
     html = urlopen(url).read()
     soup = BeautifulSoup(html, features="html.parser")
-    final_txt = []
     soup.prettify()
+    indexes = []
     sentences = soup.find_all('sen')
+    lex_sem_relations = soup.find_all('ch')
+    print("soup = ", soup)
+    for j in range(len(sentences)):
+      indexes.append(j)
+    final_txt = pd.DataFrame(columns=['sentences', 'lexico_sem_relation'], index=indexes)
     for i in range(len(sentences)):
-      text = str(sentences[i]).replace("<sen>", '')
-      text = text.replace("</sen>", '.')
-      final_txt.append(text)
-    return final_txt
+      tmp_txt = pd.DataFrame(columns=['sentences', 'lexico_sem_relation'], index=indexes)
+      text = str(sentences[i]).replace('<sen>', '')
+      text = text.replace('</sen>', '.')
+      lex_sem = str(lex_sem_relations[i]).replace('<ch>', '')
+      lex_sem = lex_sem.replace('</ch>', '')
+      lex_sem = lex_sem.replace('&gt;', '>')
+
+      final_txt['sentences'].loc[i] = text
+      final_txt['lexico_sem_relation'].loc[i] = lex_sem
+      print("TEXT SHAPE = ", np.array(text).shape)
+      print("Extract txt from html = ", final_txt)
+      print("LEN SENTENCES = ", len(sentences))
+    return final_txt, len(sentences)
 
 
 def create_dataset(nb_calls, url, type_dataset):
-  final_dataset = pd.DataFrame(columns=["sentences"])
-  dataset = []
+  indexes = []
+  final_dataset = pd.DataFrame(columns=['sentences', 'lexico_sem_relation'])
+  #tmp_dataset = []
   for i in range(nb_calls):
-    strip = extract_txt_from_html(url)
-    dataset.append(strip)
-  final_dataset['sentences'] = dataset
-  dataset_txt = final_dataset.to_csv(f"aggregate_{type_dataset}.txt", encoding="utf-8-sig")
-  return dataset_txt
+    strip, len_data = extract_txt_from_html(url)
+    print("STRIP BEFORE APPEND TO DATASET = ", strip)
+    final_dataset = pd.concat([final_dataset, strip])
+
+  print('FINAL DATASET = ', final_dataset)
+  final_dataset.to_csv(f"aggregate_{type_dataset}.txt", encoding="utf-8-sig", columns=['sentences'])
+  return final_dataset
 
 modelname = 'flaubert/flaubert_large_cased'
 tokenizer = AutoTokenizer.from_pretrained(modelname, use_fast=True)
 model = AutoModelForMaskedLM.from_pretrained(modelname)
 
-url_data = "http://www.jeuxdemots.org/intern_interpretor.php?chunks-display=1&chunk=20&verbose=0&iter=100"
+
+######   DATASET PARAMETERS #######
+
+##  CHUNK = Nombre de chunk selectionné
+## ITER  = Nombre de variantes par chunk
+
+### TAILLE MAX DE DATASET = CHUNK * ITER
+
+
+url_data = "http://www.jeuxdemots.org/intern_interpretor.php?chunks-display=1&chunk=10&verbose=0&iter=100"
 #### THIS LINK GENERATES 1300 LINES (SENTENCES)
 
-txt_train = create_dataset(3, url_data, "train")
-txt_valid = create_dataset(2, url_data, "valid")
+txt_train = create_dataset(2, url_data, "train")
+txt_valid = create_dataset(1, url_data, "valid")
 #txt_train = raw_txt_to_txt('input_raw_tr.txt', 'aggregates', 'aggregate_train')
 #txt_valid = raw_txt_to_txt('input_raw_valid.txt', 'aggregates', 'aggregate_valid')
-
+print("txt train = ", txt_train)
+print("txt train = ", txt_valid)
 ## LOAD DATASETs HERE ###
 datasets = load_dataset('text', data_files={'train': 'aggregate_train.txt', 'validation': 'aggregate_valid.txt'})
-tokenized_datasets = datasets.map(tokenize_function, batched=True, num_proc=2, remove_columns=['text'])
+tokenized_datasets = datasets.map(tokenize_function, batched=True, num_proc=4, remove_columns=['text'])
 
 lm_datasets = tokenized_datasets.map(
     group_texts,
     batched=True,
-    batch_size=512,
-    num_proc=2,
+    batch_size=1000,
+    num_proc=4,
 )
 
 print("DATASETS TRAIN = ", datasets['train'])
 print("DATASETS VALID = ", datasets['validation'])
+print("len dataset = ", np.array(datasets).shape)
 
 model_name = modelname.split("/")[-1]
 
 training_args = TrainingArguments(
     f"{model_name}-finetuned-JDM_text",
     evaluation_strategy = "epoch",
-    learning_rate=6e-5,
-    num_train_epochs=5,
-    logging_steps=50,
+    learning_rate=2e-5,
+    num_train_epochs=7,
+    logging_steps=1,
     per_device_train_batch_size=8,
-    weight_decay=0.02,
+    weight_decay=0.01,
 )
 
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)  ###  15% of sentence is masked
